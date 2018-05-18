90056530204bf740eed51c55c1526f4f1c6c032a
MJEdwin <zj726381216@live.com>
2018-05-18 18:52:31 +0800

    Add CQRS base class

--- Edwin.Infrastructure.Domain/Application/IApplicationService.cs
+++ Edwin.Infrastructure.Domain/Application/IApplicationService.cs
@@ -1,32 +1,13 @@
-﻿using Edwin.Infrastructure.Query;
-using Edwin.Infrastructure.Domain.Domain;
+﻿using Edwin.Infrastructure.Domain.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Edwin.Infrastructure.Domain.Application
 {
-    public interface IApplicationService<TEntity, TPrimaryKey>
+    public interface IApplicationService<TEntity, TPrimaryKey> : IQueries<TEntity, TPrimaryKey>, ICommand<TEntity, TPrimaryKey>
         where TEntity : IEntity<TPrimaryKey>
     {
-        #region Query
-        IQueryable<TEntity> GetAll();
-
-        IQueryable<TEntity> GetAll(params IFilter<TEntity>[] filters);
-
-        bool Exist(TPrimaryKey id);
-
-        TEntity GetById(TPrimaryKey id);
-        #endregion
-
-        #region Command
-        TEntity Create<TDTO>(TDTO dto);
-        TEntity Create(Dictionary<string, object> dictionary);
-        TEntity Upgrade<TDTO>(TPrimaryKey id, TDTO dto);
-        TEntity Upgrade(TPrimaryKey id, Dictionary<string, object> dictionary);
-        void Delete(params TPrimaryKey[] id);
-        #endregion
     }
 }
